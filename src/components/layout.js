622dcf6b778e8c7cc86cb596e3eaf626d4de4527
witocorp <hello@witocorp.com>
2021-06-24 18:36:52 +0200

    add data in gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,30 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinksItem,
   navLinksText,
+  siteTitle
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <main className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
       <nav>
         <ul className={navLinks}>
           <li className={navLinksItem}>
@@ -22,6 +35,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinksItem}>
             <Link to="/about" className={navLinksText}>
               About
+            </Link>
+          </li>
+          <li className={navLinksItem}>
+            <Link to="/blog" className={navLinksText}>
+              Blog
             </Link>
           </li>
         </ul>
