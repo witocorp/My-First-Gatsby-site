560570e42dae1bc5a20ae3cffe0462ae30687c52
witocorp <hello@witocorp.com>
2021-06-24 17:17:42 +0200

    Finish Part 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,35 @@
-import * as React from "react"
+import * as React from "react";
 import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinksItem,
+  navLinksText,
+} from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <main>
+    <main className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li className={navLinksItem}>
+            <Link to="/" className={navLinksText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinksItem}>
+            <Link to="/about" className={navLinksText}>
+              About
+            </Link>
+          </li>
         </ul>
       </nav>
-      <h1>{pageTitle}</h1>
+      <h1 className={heading}>{pageTitle}</h1>
       {children}
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
